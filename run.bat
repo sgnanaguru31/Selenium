3733098a8a40c376e8ed4ced91b77605a525376a
VMWAREM\gguru <gguru@vmware.com>
2019-02-07 16:46:33 +0530

    batch file changes

--- run.bat
+++ run.bat
@@ -5,3 +5,5 @@ echo Finished Building
 echo Running the selenium test
 call java -jar .\target\LoginTest-1.0-SNAPSHOT.jar
 echo UI test completed.
+call docker rm -f $(docker ps -a -q)
+echo website removed
